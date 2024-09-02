8102dccd0774ca6bc88a7bf670e097f822bb8c92
Docen89 <docen89@ya.ru>
2024-09-02 16:54:53 +0500

    v1

--- src/test/java/config/Converter.java
+++ src/test/java/config/Converter.java
@@ -2,17 +2,24 @@ package config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.javafaker.Faker;
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import models.lombok.LogopassModel;
+import org.junit.jupiter.api.Test;
+
 
 public class Converter {
+  Faker faker = new Faker(new Locale("EN"));
+  String firstName=faker.name().firstName();
+  String password=faker.internet().password();
 
   ObjectMapper objectMapper = new ObjectMapper();
-
+@Test
   void pojoToJsonString() throws IOException {
     objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-    LogopassModel logpass = new LogopassModel("Taras", "123");
+    LogopassModel logpass = new LogopassModel(firstName, password);
     objectMapper.writeValue(new File("src/test/resources/Logopass.json"), logpass);
   }
 }
